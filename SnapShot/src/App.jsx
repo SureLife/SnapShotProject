528ee7c5bd27f60c595b3112ac896b879356e7f6
daniel <daniel@DESKTOP-VNOQHDA>
2023-09-07 09:33:09 +0200

    Got the Navbar functionality working

--- SnapShot/src/App.jsx
+++ SnapShot/src/App.jsx
@@ -8,15 +8,14 @@ import Cards from "./components/Cards";
 function App() {
   const [images, setPhotos] = useState([]);
   const [error, setError] = useState(null);
-  const [query, setQuery] = useState("hello"); //todo
+  const [query, setQuery] = useState("Welcome");
 
   // fetch data from API
   async function handleSearch(text) {
+    
     setQuery(text);
   }
-  
     useEffect(() => {
-      //const query = "hello"; //todo==> to modify
 
       fetch(
         `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${
@@ -27,27 +26,24 @@ function App() {
         .then((data) => {
           console.log(data);
           setPhotos(data.photos.photo);
-        }) //!remember to delete console.log(data)
+        }) 
         .catch((error) => {
           setError(error);
         });
         console.log(query);
-    }, [query]); //todo==> modify dependency?
+    }, [query]); 
   
   return (
     <BrowserRouter>
       <SearchBar handleSearch={handleSearch}/>
-
       <NavBar handleSearch={handleSearch}/>
-
       <Routes>
         {/* path for the pages */}
-        <Route path="/mountain" element={<div>Mountain Page</div>} />
-        <Route path="/beaches" element={<div>Beaches Page</div>} />
-        <Route path="/birds" element={<div>Birds Page</div>} />
-        <Route path="/food" element={<div>Food Page</div>} />
+        <Route path="/mountain" element={<div>Mountain Pictures</div>} />
+        <Route path="/beaches" element={<div>Beaches Pictures</div>} />
+        <Route path="/birds" element={<div>Birds Pictures</div>} />
+        <Route path="/food" element={<div>Food Pictures</div>} />
       </Routes>
-
       <Cards images={images} />
     </BrowserRouter>
   );
--- SnapShot/src/components/NavBar.jsx
+++ SnapShot/src/components/NavBar.jsx
@@ -1,18 +1,21 @@
-
+import React, { useState } from "react";
 import { Link } from 'react-router-dom'
 
 
 function NavBar({handleSearch}) {
-
-
+  const [search, setSearch] = useState(""); 
+  
+    
+    handleSearch(search);
+  
 
     return (
     <nav className="NavContainer">
       <ul>
-        <li><Link to="/mountain">Mountain</Link></li>
-        <li><Link to="/beaches">Beaches</Link></li>
-        <li><Link to="/birds">Birds</Link></li>
-        <li><Link to="/food">Food</Link></li>
+        <li onClick={(e) => setSearch("mountain")}><Link to="/mountain">Mountain</Link></li>
+        <li onClick={(e) => setSearch("beaches")}><Link to="/beaches">Beaches</Link></li>
+        <li onClick={(e) => setSearch("birds")}><Link to="/birds">Birds</Link></li>
+        <li onClick={(e) => setSearch("food")}><Link to="/food">Food</Link></li>
       </ul>
     </nav>
     )
